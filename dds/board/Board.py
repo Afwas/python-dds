98c4a1d5dbf5d91f870c8ef32401c41efc5faedd
Foppe Hemminga <foppe@hemminga.net>
2015-08-31 07:04:36 +0200

    First start at unittest

--- dds/board/Board.py
+++ dds/board/Board.py
@@ -4,11 +4,3 @@ class Board():
     def __init__(self):
         pass
 
-class PBN(Board):
-    def __inti__(self, PBN):
-        self._raw_pbn = self._clean_pbn(PBN)
-
-    def _clean_pbn(self, PBN):
-        """Searches for the deal within the string
-        starting with the character for the first hand"""
-        return PBN[PBN.find(":") - 1:]
---
+++ dds/test/ímport untittest.py
@@ -0,0 +1,8 @@
+ímport untittest
+
+class PBNTest(unittest.TestCase):
+	def setUp(self):
+		testPBN = new 
+
+if __name__=="__main__":
+	unittest.main()
