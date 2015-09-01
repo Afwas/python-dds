cb986f6a0a121960df94b55204bc9ca12d580ac1
Foppe Hemminga <foppe@hemminga.net>
2015-09-01 05:09:04 +0200

    Added __pycache__ to .gitignore

--- dds/board/PBN.py
+++ dds/board/PBN.py
@@ -1,14 +1,16 @@
 class PBN:
-    def __init__(self, PBN_text):
-        self._raw_pbn = self._clean_pbn(PBN_text)
+    def __init__(self, PBN_input):
+        """ @TODO At this point the class assumes starting PBN with N"""
+        self._raw_pbn = PBN_input
 
-    def _clean_pbn(self, PBN_text):
-        """Searches for the deal within the string
-        starting with the character for the first hand"""
-        return PBN_text[PBN_text.find(":") - 1:]
+    def get_cards(self):
+        return self._raw_pbn[3:-1]
 
-    def getDealer(self):
-    	return self._raw_pbn[0]
+    def get_dealer(self):
+    	return self._raw_pbn[1]
 
     def _split(self):
     	return self._raw_pbn.split(' ')
+
+    def get_hand(self, hand):
+        return self.get_cards().split(' ')[hand]
--- dds/test/PBN_test.py
+++ dds/test/PBN_test.py
@@ -6,8 +6,17 @@ class PBNTest(unittest.TestCase):
 		# print(dir(PBN))
 		self.testPBN = PBN.PBN("[N:.63.AKQ987.A9732 A8654.KQ5.T.QJT6 J973.J98742.3.K4 KQT2.AT.J6542.85]")
 
+	def test_clean_pbn(self):
+		self.assertEqual(len(self.testPBN.get_cards()), 67)
+
 	def test_dealer(self):
-		self.assertEqual(self.testPBN.getDealer(), 'N')
+		self.assertEqual(self.testPBN.get_dealer(), 'N')
+
+	def test_hands(self):
+		self.assertEqual(self.testPBN.get_hand(0), ".63.AKQ987.A9732")
+		self.assertEqual(self.testPBN.get_hand(1), "A8654.KQ5.T.QJT6")
+		self.assertEqual(self.testPBN.get_hand(2), "J973.J98742.3.K4")
+		self.assertEqual(self.testPBN.get_hand(3), "KQT2.AT.J6542.85")
 
 if __name__== "__main__":
 	unittest.main()
