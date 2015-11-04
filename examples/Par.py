7ae4dc5501c34c9d8c90a3bfd25fe7272f6df9e2
Foppe Hemminga <foppe@hemminga.net>
2015-11-04 17:05:30 +0100

    Revisisted Par.py

--- examples/Par.py
+++ examples/Par.py
@@ -3,21 +3,27 @@ import hands
 import functions
 import ctypes
 
-for handno in range(0, 3):
-    DDTable = dds.ddTableResults()
-    pres = dds.parResults()
-    myDDTable = ctypes.pointer(DDTable)
-    myPres = ctypes.pointer(pres)
+DDtable = dds.ddTableResults()
+pres = dds.parResults()
 
-    functions.SetTable(myDDTable, handno)
-    res = dds.Par(myDDTable, myPres, hands.vul[handno])
+line = ctypes.create_string_buffer(80)
+
+dds.SetMaxThreads(0)
+
+for handno in range(0, 3):
+    functions.SetTable(ctypes.pointer(DDtable), handno)
+    res = dds.Par(
+        ctypes.pointer(DDtable),
+        pres,
+        hands.vul[handno])
+
     if res != dds.RETURN_NO_FAULT:
         dds.ErrorMessage(res, line)
-        print("DDS error: {}".format(line))
+        print("DDS error: {}".format(line.value.decode("utf-8")))
 
-    match = functions.ComparePar(myPres, handno)
-    print("Par, hand {}: {}".format(handno + 1, "OK" if match else "ERROR"))
-    print("")
+    match = functions.ComparePar(ctypes.pointer(pres), handno)
 
-    functions.PrintTable(myDDTable)
-    functions.PrintPar(myPres)
+    print("Par, hand {}: {}\n".format(handno + 1, "OK" if match else "ERROR"))
+
+    functions.PrintTable(ctypes.pointer(DDtable))
+    functions.PrintPar(ctypes.pointer(pres))
