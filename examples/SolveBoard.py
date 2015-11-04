0c4e2f68aaa7b310e35425b2f196d03acc499bc7
Foppe Hemminga <foppe@hemminga.net>
2015-11-04 20:09:13 +0100

    Revisited all files.

--- examples/SolveBoard.py
+++ examples/SolveBoard.py
@@ -7,13 +7,9 @@ import ctypes
 
 dl = dds.deal()
 fut2 = dds.futureTricks()
-myFut2 = ctypes.pointer(fut2)
 fut3 = dds.futureTricks()
-myFut3 = ctypes.pointer(fut3)
 
 threadIndex = 0
-#line = " " * 80
-#myLine = ctypes.pointer(line)
 line = ctypes.create_string_buffer(80)
 
 dds.SetMaxThreads(0)
@@ -34,24 +30,30 @@ for handno in range(3):
 		for s in range(dds.DDS_SUITS):
 			dl.remainCards[h][s] = hands.holdings[handno][s][h]
 
-	target = -1 # No target; find maximum results
-	solutions = 3 # Return 3 solutions
-	mode = 0
-	res = dds.SolveBoard(dl, target, solutions, mode, myFut3, threadIndex)
+        ###
+        # target: Either a number <= 13 or 0 or -1: The 'target' number of tricks to make
+        # solutions: 1 - 3: 1 means return 1 solution, 
+        #   2 means return all maximum solutions, 3 means all solutions
+        # mode: 0 or 1. Just for internal use. 0 is fine here.
+        ###
+	target = -1 # No target; find all results
+	solutions = 3 # Return all solutions
+	mode = 0 # The way dds internally handles the last trick
+	res = dds.SolveBoard(dl, target, solutions, mode, ctypes.pointer(fut3), threadIndex)
 
 	if res != dds.RETURN_NO_FAULT:
 		dds.ErrorMessage(res, line)
 		print("DDS error: {}".format(line.value.decode("utf-8")))
 
-	match3 = functions.CompareFut(myFut3, handno, solutions)
+	match3 = functions.CompareFut(ctypes.pointer(fut3), handno, solutions)
 
-	solutions = 2
-	res = dds.SolveBoard(dl, target, solutions, mode, myFut2, threadIndex)
+	solutions = 2 # Return only the optmial solutions
+	res = dds.SolveBoard(dl, target, solutions, mode, ctypes.pointer(fut2), threadIndex)
 	if res != dds.RETURN_NO_FAULT:
 		dds.ErrorMessage(res, line)
 		print("DDS error: {}".format(line.value.decode("utf-8")))
 
-	match2 = functions.CompareFut(myFut2, handno, solutions)
+	match2 = functions.CompareFut(ctypes.pointer(fut2), handno, solutions)
 
 	line = "SolveBoard, hand {}: solutions 3 {}, solutions 2 {}".format( \
 		handno + 1, "OK" if match3 else "ERROR", "OK" if match2 else "ERROR")
@@ -59,6 +61,6 @@ for handno in range(3):
 	functions.PrintHand(line, dl.remainCards)
 
 	line = "solutions == 3"
-	functions.PrintFut(line, myFut3)
+	functions.PrintFut(line, ctypes.pointer(fut3))
 	line = "solutions == 2"
-	functions.PrintFut(line, myFut2)
+	functions.PrintFut(line, ctypes.pointer(fut2))
